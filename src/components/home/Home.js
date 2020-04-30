3ec8263a800957cd6813f7945ff7242bfd91b1e1
GunturThunder <gangkasa888@gmail.com>
2020-04-30 13:44:11 +0700

    first-commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text, StatusBar } from 'react-native'
+import QuickInfo from '../quickInfo/QuickInfo'
 
 class Home extends Component {
     render() {
         return (
             <View>
                 <StatusBar backgroundColor="#000" barStyle="light-content" />
-                <Text>Home</Text>
+                <QuickInfo />
             </View>
         )
     }
---
+++ src/components/quickInfo/QuickInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class QuickInfo extends Component {
+    render(){
+        return(
+            <View>
+                <Text>QuickInfo</Text>
+            </View>
+        )
+    }
+}
+
+export default QuickInfo
