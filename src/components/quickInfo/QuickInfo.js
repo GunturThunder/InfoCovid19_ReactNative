49cf933c00ac6763c570e48c0e876014e2892f46
guntur_thunder <gangkasa888@gmail.com>
2020-06-19 08:02:50 +0700

    push notif

--- src/components/quickInfo/QuickInfo.js
+++ src/components/quickInfo/QuickInfo.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import PushNotification from "react-native-push-notification"
+import { Button } from 'native-base';
 
 class QuickInfo extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        PushNotification.configure({
+            onRegister: function (token) {
+                console.log("TOKEN:", token);
+            },
+            onNotification: function (notification) {
+                console.log("NOTIFICATION:", notification);
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true,
+            },
+            popInitialNotification: true,
+            requestPermissions: true,
+        });
+    }
+
+    sendNotif = () => {
+        PushNotification.localNotification({
+            title: "My Notification Title", // (optional)
+            message: "My Notification Message", // (required)
+          });
+    }
+
+    render() {
+        return (
             <View>
                 <Text>QuickInfo</Text>
+                <Button onPress={()=>this.sendNotif()} >
+                    <Text>Click Here</Text></Button>
             </View>
         )
     }
